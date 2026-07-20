1da664099868aa32de34ceb90b59105d2073b24c
GhanaSproutsMath <ghanasprouts@gmail.com>
2026-07-20 08:29:04 -0700

    3 column setup commit

---
+++ demo/basic_compute.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+var rd = RenderingServer.create_local_rendering_device();
+func _ready() -> void:
+	var shader_file := load("res://compute_example.glsl")
+	var shader_spirv: RDShaderSPIRV = shader_file.get_spirv()
+	var shader := rd.shader_create_from_spirv(shader_spirv)
+
+	var input := PackedFloat32Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+	var input_bytes := input.to_byte_array()
+
+	# Create a storage buffer that can hold our float values.
+	# Each float has 4 bytes (32 bit) so 10 x 4 = 40 bytes
+	var buffer := rd.storage_buffer_create(input_bytes.size(), input_bytes)
+	# Create a uniform to assign the buffer to the rendering device
+	var uniform := RDUniform.new()
+	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	uniform.binding = 0 # this needs to match the "binding" in our shader file
+	uniform.add_id(buffer)
+	var uniform_set := rd.uniform_set_create([uniform], shader, 0) # the last parameter (the 0) needs to match the "set" in our shader file
+	
+	var pipeline := rd.compute_pipeline_create(shader)
+	var compute_list := rd.compute_list_begin()
+	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
+	# this is where our triangle values would go
+	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
+	rd.compute_list_dispatch(compute_list, 5, 1, 1)
+	rd.compute_list_end()	
